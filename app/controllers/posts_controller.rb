051a23538eb582d30f3fc444387d89582679f657
Szymon Fiedler <fidelski@gmail.com>
2013-02-17 15:53:45 +0100

    Optimized posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,83 +1,58 @@
 class PostsController < ApplicationController
+  before_filter :authenticate_user!
+  before_filter :find_post, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html, :json
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @posts }
-    end
+    @posts = current_user.posts
+    respond_with(@posts)
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @post = Post.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @post }
-    end
+    respond_with(@post)
   end
 
   # GET /posts/new
   # GET /posts/new.json
   def new
-    @post = Post.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @post }
-    end
+    @post = current_user.posts.build
+    respond_with(@post)
   end
 
   # GET /posts/1/edit
   def edit
-    @post = Post.find(params[:id])
+    respond_with(@post)
   end
 
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(params[:post])
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render json: @post, status: :created, location: @post }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    @post = current_user.posts.build(params[:post])
+    flash[:notice] = 'Post was successfully created.' if @post.save
+    respond_with(@post)
   end
 
   # PUT /posts/1
   # PUT /posts/1.json
   def update
-    @post = Post.find(params[:id])
-
-    respond_to do |format|
-      if @post.update_attributes(params[:post])
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
-    end
+    flash[:notice] = 'Post was successfully updated.' if @post.update_attributes(params[:post])
+    respond_with(@post)
   end
 
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    @post = Post.find(params[:id])
     @post.destroy
+    respond_with(@post)
+  end
 
-    respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
-    end
+  private
+
+  def find_post
+    @post = current_user.posts.find(params[:id])
   end
 end
